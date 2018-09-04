538aec89a079c5190422df4107853e3d563c2efe
Gavriil Krendelev <krendelev@GKmbp.local>
2018-09-05 00:59:29 +0300

    some minor corrections according to review

--- pprint_json.py
+++ pprint_json.py
@@ -3,20 +3,22 @@ import json
 
 
 def load_data(filepath):
-    with open(filepath) as handle:
-        return json.load(handle)
+    with open(filepath) as file_handler:
+        return json.load(file_handler)
 
 
-def pretty_print_json(unformatted_data):
-    return json.dumps(unformatted_data, ensure_ascii=False, indent=4)
+def pretty_print_json(data_object):
+    return json.dumps(data_object, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
     try:
-        unformatted_json = load_data(sys.argv[1])
-    except (IndexError, IOError):
-        print("There is no file to prettify")
+        data_from_json = load_data(sys.argv[1])
+    except IndexError:
+        print('Please specify path to JSON file')
+    except FileNotFoundError:
+        print('File not found')
     except ValueError:
-        print("Not a valid JSON file")
+        print('Not a valid JSON file')
     else:
-        print(pretty_print_json(unformatted_json))
+        print(pretty_print_json(data_from_json))
